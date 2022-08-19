24bff9a291c441da6a113b5a11b445e892dbf4e2
chayanchowdhury <chayanchowdhury@gmail.com>
2022-08-19 10:47:25 +0530

    showing All  element in An Array using For Loop

---
+++ ForEachLoopUseInArray.java
@@ -0,0 +1,14 @@
+public class ForEachLoopUseInArray{
+
+    public static void main(String[] args) {
+
+        int [] array={10,20,30,40,50};
+
+        for (int i : array) {
+
+         System.out.println("Display All element in array"+" :  " +i);
+            
+        }
+
+    }
+}
