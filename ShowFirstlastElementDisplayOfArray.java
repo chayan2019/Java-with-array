a9ec61f1c882c384970c6d03ef772f07dd085083
chayanchowdhury <chayanchowdhury@gmail.com>
2022-10-17 00:55:15 +0530

    showing First and last element in An Array

---
+++ ShowFirstlastElementDisplayOfArray.java
@@ -0,0 +1,13 @@
+public class ShowFirstlastElementDisplayOfArray{
+
+    public static void main(String[] args) {
+
+        int [] array={10,20,30,40,50};
+
+        System.out.println("Display First element in array"+" :  " +array[0]);
+
+        System.out.println("Display last the element in array"+" :  " +array[array.length-1]);
+         
+        
+    }
+}
