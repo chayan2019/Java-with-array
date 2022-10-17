b3aea733a69cc9dbdc7344a46977a37e709b34db
chayanchowdhury <chayanchowdhury@gmail.com>
2022-10-18 00:41:32 +0530

    Displaying Element in An Array

---
+++ ListOfElementDisplayArray.java
@@ -0,0 +1,12 @@
+public class ListOfElementDisplayArray{
+
+    public static void main(String[] args) {
+
+        int [] array={10,20,30,40,50};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Display all the element in array"+" :  " +array[i]);
+        }
+        
+    }
+}
