05facf9107d0c9970ccca5060b0d8f83ad8c4bd4
Chayan <chayan@Chayans-MacBook-Pro.local>
2022-10-17 19:05:29 +0530

    Java Program to Find the Number of Elements in an Array

---
+++ FindElementInArray.java
@@ -0,0 +1,12 @@
+public class FindElementInArray{
+
+    public static void main(String[] args) {
+
+        int [] array={10,20,30,40,50};
+        int total=0;
+        for(int i=0;i<array.length;i++){
+             total++;
+        }
+        System.out.println(" Find the Number of Elements in an Array"+" :  " +total);
+    }
+}
